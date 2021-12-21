f1889810865770e53d657a95e731d314a80ce0c8
ngasqus <agngwb@gmail.com>
2021-12-21 14:52:07 +0700

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -45,9 +45,9 @@ ip_address=$(get_ip_addresses &)
 echo -e "Uptime : "$UPTIME >> $MSG_FILE
 echo -e "CPU Temp : "$board_temp" C" >> $MSG_FILE
 echo -e "IP : "$ip_address >> $MSG_FILE
-echo -e "Pool address : "$(cat $HOME_DIR/miner.log | grep "Starting on" | awk '{print $5}')"" >> $MSG_FILE
-echo -e "Hashrate : "$(tail -1 $HOME_DIR/miner.log | grep "accept" | awk '{print $7}')" kH/s" >> $MSG_FILE
-echo -e "Last share : "$(tail -1 $HOME_DIR/miner.log | grep "accept" | awk '{print $4}') >> $MSG_FILE
+echo -e "Pool address : "$(cat $HOME_DIR/autorun.log | grep "Starting on" | awk '{print $5}')"" >> $MSG_FILE
+echo -e "Hashrate : "$(tail -1 $HOME_DIR/autorun.log | grep "accept" | awk '{print $7}')" kH/s" >> $MSG_FILE
+echo -e "Last share : "$(tail -1 $HOME_DIR/autorun.log | grep "accept" | awk '{print $4}') >> $MSG_FILE
 # Send Notif Telegram
 TOKEN=2105826072:AAH2ULLlIXsVxWdHP8p7VqjRVMmc7hIGXE8
 CHAT_ID=(306773502) #ID_TELEGRAM diisi dengan id yang akan menerima notifnya.
